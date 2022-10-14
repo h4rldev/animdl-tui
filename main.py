ead40457e11e4e9318d38813a55f03c0c6007d0b
h4rl <0derp05@gmail.com>
2022-10-14 07:30:15 +0200

    eh

--- main.py
+++ main.py
@@ -1,73 +1,21 @@
-import pick, ctypes, sys, os
+import ctypes, sys, os
+from bullet import Bullet, Check, YesNo, Input, Date, CheckDependencies, Password, ScrollBar
 from msvcrt import getch as getkey
-from termcolor import colored
-from click import clear
+from click import clear, prompt
 
-os.system('color')
+if os.name == 'nt':
+    os.system('color')
+
+
+def continue1():
+    print("\n Press any key to continue");
+    getkey();
 
 def leave():
-    exit();
+    os.system("exit");
 
 def settings():
-    def provider():
-        global provider
-        title = 'Choose Provider: '
-        options = ['animixplay', 'haho (NSFW)', 'gogoanime', 'tenshi', 'animepahe','allanime', 'Exit']
-        indicator = 'Provider -> •'
-        option, index = pick.pick(options, title, indicator)
-        amogus = index
-
-        if amogus == 0:
-            provider = 'animixplay'
-            print("The provider has been set to", provider)
-            getkey();
-        elif amogus == 1:
-            provider = 'haho'
-            print("The provider has been set to", provider)
-            getkey();
-        elif amogus == 2:
-            provider = 'gogoanime'
-            print("The provider has been set to", provider)
-            getkey();
-        elif amogus == 3:
-            provider = 'tenshi'
-            print("The provider has been set to", provider)
-            getkey();
-        elif amogus == 4:
-            provider = 'animepahe'
-            print("The provider has been set to", provider)
-            getkey();
-        elif amogus == 5:
-            provider = 'allanime'
-            print("The provider has been set to\""+provider+"\"")
-            getkey();
-        else:
-            settings();
-        getkey();
-    def quality():
-        getkey();
-    def range():
-        getkey();
-    def directory():
-        getkey();
-    def leave():
-        menu();
-    title = 'Settings: '
-    options = ['Provider', 'Quality', 'Range', 'Directory', 'Exit']
-    indicator = '•'
-    option, index = pick.pick(options, title, indicator)
-    amogus = index
-
-    if amogus == 0:
-        provider();
-    elif amogus == 1:
-        quality();
-    elif amogus == 2:
-        range();
-    elif amogus == 3:
-        directory();
-    else:
-        leave();
+    print("yes")
 
 def update():
     def is_admin():
@@ -76,12 +24,16 @@ def update():
         except:
             return False
 
-    if is_admin():
-        os.system("animdl update")
-        getkey();
+    if os.name == 'nt':
+        if is_admin():
+            os.system("animdl update")
+        else:
+            # Re-run the program with admin rights
+            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+            os.system("powershell.exe %s" % (__file__))
     else:
-        # Re-run the program with admin rights
-        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+        print("I can't test this with linux rn. Run \'sudo animdl update to update.\'")
+        exit();
 
 def stream():
     getkey();
@@ -99,47 +51,47 @@ def download():
     getkey();
 
 def menu():
-    title = 'What do you want to do?: '
-    options = ['[1.] Download', '[2.] Grab', '[3.] Schedule', '[4.] Search', '[5.] Stream', '[6.] Update', '[7.]Settings', '[8.] Exit']
-    indicator = 'Main Menu -> •'
-    option, index = pick.pick(options, title, indicator)
-    amogus = index
-
-    if amogus == 0:
-        download();
-    elif amogus == 1:
-        grab();
-    elif amogus == 2:
-        schedule();
-    elif amogus == 3:
-        search();
-    elif amogus == 4:
-        stream();
-    elif amogus == 5:
-        update();
-    elif amogus == 6:
-        settings();
-    else:
-        leave();
+    cli = Bullet(bullet = ">> ", prompt = " Choose Action:", choices = ['1. Download', '2. Grab', '3. Schedule', '4. Search', '5. Stream', '6. Update', '7. Settings', '8. Exit'])
+    result = cli.launch()
+    match result:
+        case "1. Download":
+            clear();
+            download();
+        case "2. Grab":
+            clear();
+            grab();
+        case "3. Schedule":
+            clear();
+            schedule();
+        case "4. Search":
+            clear();
+            search();
+        case "5. Stream":
+            clear();
+            stream();
+        case "6. Update":
+            clear();
+            update();
+        case "7. Settings":
+            clear();
+            settings();
+        case "8. Exit":
+            clear();
+            leave();
 
 def is_admin():
-    try:
-        return ctypes.windll.shell32.IsUserAnAdmin()
-    except:
+    if os.name == 'nt':
+        try:
+            return ctypes.windll.shell32.IsUserAnAdmin()
+        except:
+            return False
+    else:
         return False
 
-if is_admin():
-        title = 'Do you want to update?: '
-        options = ['Yes', 'No']
-        indicator = '•'
-        pick.pick(options)
-        option, index = pick.pick(options, title, indicator)
-        amogus = index
 
-        if amogus == 0:
-            update();
-        else:
-            menu();
+if is_admin():
+    os.system("animdl update");
+    exit();
 
 if __name__ == '__main__':
     menu();
