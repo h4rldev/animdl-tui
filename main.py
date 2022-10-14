076f60747e04bab933c8d205238542e59bfa0c7a
h4rldev <h4rldev@proton.me>
2022-10-14 19:39:31 +0200

    yes amogus

--- main.py
+++ main.py
@@ -1,86 +1,112 @@
-import ctypes, sys, os
+import ctypes, sys, platform, os
+from colored import fg, attr
 from bullet import Bullet, Check, YesNo, Input, Date, CheckDependencies, Password, ScrollBar
 from msvcrt import getch as getkey
-from click import clear, prompt
+from click import clear
 
-if os.name == 'nt':
-    os.system('color')
+blue = fg("blue")
+reset = attr("reset")
 
+animdl1 = " ___  _ _  _  __ _        __   _"
+animdl2 = "| . || \ || ||  \ \  ___ | . \| |"
+animdl3 = "|   ||   || ||     ||___|| | || |_"
+animdl4 = "|_|_||_\_||_||_|_|_|     |___/|___|"
 
 def continue1():
     print("\n Press any key to continue");
     getkey();
 
-def leave():
-    os.system("exit");
+def main():
+    def leave():
+        os.system("exit");
 
-def settings():
-    print("yes")
+    def settings():
+        print("yes")
 
-def update():
-    def is_admin():
-        try:
-            return ctypes.windll.shell32.IsUserAnAdmin()
-        except:
-            return False
+    def update():
+        match platform.system:
+            case 'Windows':
+                if is_admin():
+                    os.system("animdl update")
 
-    if os.name == 'nt':
-        if is_admin():
-            os.system("animdl update")
-        else:
-            # Re-run the program with admin rights
-            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
-            os.system("powershell.exe %s" % (__file__))
-    else:
-        print("I can't test this with linux rn. Run \'sudo animdl update to update.\'")
-        exit();
+                else:
+                    # Re-run the program with admin rights
+                    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+                    os.system("powershell.exe %s" % (__file__))
 
-def stream():
-    getkey();
+            case 'Linux':
+                print("I can't test this with linux rn.",
+                "Run \'sudo animdl update to update.\'")
+                exit();
 
-def search():
-    getkey();
+            case 'Darwin':
+                print("If you really use mac you have to contribute, I don't own a mac because I don't suck")
+                exit();
 
-def schedule():
-    getkey();
+    def stream():
+        getkey();
 
-def grab():
-    getkey();
+    def search():
+        getkey();
 
-def download():
-    getkey();
+    def schedule():
+        getkey();
 
-def menu():
-    cli = Bullet(bullet = ">> ", prompt = " Choose Action:", choices = ['1. Download', '2. Grab', '3. Schedule', '4. Search', '5. Stream', '6. Update', '7. Settings', '8. Exit'])
-    result = cli.launch()
-    match result:
-        case "1. Download":
-            clear();
-            download();
-        case "2. Grab":
-            clear();
-            grab();
-        case "3. Schedule":
-            clear();
-            schedule();
-        case "4. Search":
-            clear();
-            search();
-        case "5. Stream":
-            clear();
-            stream();
-        case "6. Update":
-            clear();
-            update();
-        case "7. Settings":
-            clear();
-            settings();
-        case "8. Exit":
-            clear();
-            leave();
+    def grab():
+        getkey();
+
+    def download():
+        getkey();
+
+    def menu():
+        print(blue, animdl1, reset)
+        print(blue, animdl2, reset)
+        print(blue, animdl3, reset)
+        print(blue, animdl4, reset, "\n")
+        menu = Bullet(
+            bullet = ">> ",
+            prompt = "Choose Action:",
+            choices = [
+                '1. Download',
+                '2. Grab',
+                '3. Schedule',
+                '4. Search',
+                '5. Stream',
+                '6. Update',
+                '7. Settings',
+                '8. Exit'
+            ])
+
+        result = menu.launch()
+        match result:
+            case "1. Download":
+                clear();
+                download();
+            case "2. Grab":
+                clear();
+                grab();
+            case "3. Schedule":
+                clear();
+                schedule();
+            case "4. Search":
+                clear();
+                search();
+            case "5. Stream":
+                clear();
+                stream();
+            case "6. Update":
+                clear();
+                update();
+            case "7. Settings":
+                clear();
+                settings();
+            case "8. Exit":
+                clear();
+                leave();
+    menu();
 
 def is_admin():
-    if os.name == 'nt':
+    if platform.system == 'Windows':
         try:
             return ctypes.windll.shell32.IsUserAnAdmin()
         except:
@@ -88,10 +114,10 @@ def is_admin():
     else:
         return False
 
-
 if is_admin():
     os.system("animdl update");
     exit();
 
 if __name__ == '__main__':
-    menu();
+    clear();
+    main();
