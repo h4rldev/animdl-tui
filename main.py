81e57ab7c6c60da5cbb1c2760be98c8c4b44239f
h4rldev <h4rldev@proton.me>
2022-10-28 21:00:53 +0200

    adding config and did some things ig

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import sys, platform, os, ctypes, cursor
 from colored import fg, attr
-from bullet import Bullet, colors, Check
+from bullet import Bullet, colors
 from msvcrt import getch as getkey
 from click import clear
+from configparser import ConfigParser
+global config
+global file
+file = "animdl-tui\\config.ini"
+config = ConfigParser()
+config.read(file)
 
 cyan = fg("cyan")
 reset = attr("reset")
 bold = attr("bold")
+green = fg("green")
+red = fg("red")
 
-animdl1 = " ___  _ _  _  _  _   __   _"
-animdl2 = "| . || \ || ||  \ \ | . \| |"
-animdl3 = "|   ||   || ||     || | || |_"
-animdl4 = "|_|_||_\_||_||_|_|_||___/|___|"
-animdl5 = "=============================="
+
+animdl1 = "    ___    _   ________  _______  __       ________  ______"
+animdl2 = "   /   |  / | / /  _/  |/  / __ \/ /      /_  __/ / / /  _/"
+animdl3 = "  / /| | /  |/ // // /|_/ / / / / /  ______/ / / / / // /  "
+animdl4 = " / ___ |/ /|  // // /  / / /_/ / /__/_____/ / / /_/ // /   "
+animdl5 = "/_/  |_/_/ |_/___/_/  /_/_____/_____/    /_/  \____/___/ "
 
 def continue1():
     print("\n Press any key to continue");
@@ -23,54 +32,170 @@ def main():
     def leave():
         os.system("exit");
 
+    def special():
+        print(-1)
+
+    def range():
+        print(-1)
+
+    def quality():
+        print(-1)
+
+    def player():
+        print(-1)
+
+    def provider():
+        print(-1)
+
     def toggle():
-        Check(checked=True)
+        clear();
+        file = "animdl-tui\\config.ini"
+        config = ConfigParser()
+        config.read(file)
+
+        if config['toggles']['provider'] == 'False':
+            providerstatuscolor = red
+        else:
+            providerstatuscolor = green
+
+        if config['toggles']['special'] == 'False':
+            specialstatuscolor = red
+        else:
+            specialstatuscolor = green
+
+        if config['toggles']['range'] == 'False':
+            rangestatuscolor = red
+        else:
+            rangestatuscolor = green
+
+        if config['toggles']['quality'] == 'False':
+            qualitystatuscolor = red
+        else:
+            qualitystatuscolor = green
+        print("%s%s%s\n%s\n%s\n%s\n%s\n%s" % (bold, cyan, animdl1, animdl2, animdl3, animdl4, animdl5, reset));
+
+        print("           +-----------------------+")
+        print("           | PROVIDER      =", providerstatuscolor, "\b{:5}".format(config['toggles']['provider']), reset, "\b|")
+        print("           | QUALITY       =", qualitystatuscolor, "\b{:5}".format(config['toggles']['quality']), reset, "\b|")
+        print("           | RANGE         =", rangestatuscolor, "\b{:5}".format(config['toggles']['range']), reset, "\b|")
+        print("           | SPECIAL RANGE =", specialstatuscolor, "\b{:5}".format(config['toggles']['special']), reset, "\b|")
+        print("           +-----------------------+")
+        modules = Bullet(
+            prompt = "%s                 Enable modules: %s \n" % (bold, reset),
+            choices = [
+                "Provider",
+                "Quality",
+                "Range",
+                "Special",
+                "Exit"
+            ],
+            bullet = "",
+            margin = 0,
+            align = 20,
+            word_on_switch=colors.foreground["white"],
+            background_on_switch=colors.background["cyan"]
+        )
+        result = modules.launch()
+        print("                    +------+")
+        match result:
+            case "Provider":
+                if config['toggles']['provider'] == 'True':
+                    print("The PROVIDER MODULE is now False")
+                    config.set('toggles', 'provider', 'False')
+                    with open(file, 'w') as configfile:
+                        config.write(configfile)
+                    toggle();
+                else:
+                    print("The PROVIDER MODULE is now True")
+                    config.set('toggles', 'provider', 'True')
+                    with open(file, 'w') as configfile:
+                        config.write(configfile)
+                    toggle();
+            case "Quality":
+                if config['toggles']['quality'] == 'True':
+                    print("The QUALITY MODULE is now False")
+                    config.set('toggles', 'quality', 'False')
+                    with open(file, 'w') as configfile:
+                        config.write(configfile)
+                    toggle();
+                else:
+                    print("The QUALITY MODULE is now True")
+                    config.set('toggles', 'quality', 'True')
+                    with open(file, 'w') as configfile:
+                        config.write(configfile)
+                    toggle();
+            case "Range":
+                if config['toggles']['range'] == 'True':
+                    print("The RANGE MODULE is now False")
+                    config.set('toggles', 'range', 'False')
+                    with open(file, 'w') as configfile:
+                        config.write(configfile)
+                    toggle();
+                else:
+                    print("The RANGE MODULE is now True")
+                    config.set('toggles', 'range', 'True')
+                    with open(file, 'w') as configfile:
+                        config.write(configfile)
+                    toggle();
+            case "Special":
+                if config['toggles']['special'] == 'True':
+                    print("The SPECIAL RANGE MODULE is now False")
+                    config.set('toggles', 'special', 'False')
+                    with open(file, 'w') as configfile:
+                        config.write(configfile)
+                    toggle();
+                else:
+                    print("The SPECIAL RANGE MODULE is now True")
+                    config.set('toggles', 'special', 'True')
+                    with open(file, 'w') as configfile:
+                        config.write(configfile)
+                    toggle();
+            case "Exit":
+                return 0;
 
     def settings():
+        clear();
         print("%s%s%s\n%s\n%s\n%s\n%s\n%s" % (bold, cyan, animdl1, animdl2, animdl3, animdl4, animdl5, reset));
         settings = Bullet(
-            prompt = "%s        -Settings:- %s \n" % (bold, reset),
+            prompt = "%s                     Settings:  %s \n" % (bold, reset),
             choices = [
-                '- Toggle   -'
-                '- Provider -'
-                '- Player   -'
-                '- Quality  -'
-                '- Range    -'
-                '- Special  -'
+                '- Toggle   -',
+                '- Provider -',
+                '- Player   -',
+                '- Quality  -',
+                '- Range    -',
+                '- Special  -',
                 '- Exit     -'
             ],
             bullet = "",
             margin = 0,
-            align = 8,
+            align = 20,
             word_on_switch=colors.foreground["white"],
             background_on_switch=colors.background["cyan"]
         )
         result = settings.launch()
         match result:
-            case '- Download -':
+            case '- Toggle   -':
                 clear();
-                download();
-            case '- Stream   -':
+                toggle();
+            case '- Provider -':
                 clear();
                 stream();
-            case '- Search   -':
+            case '- Player   -':
                 clear();
                 search();
-            case '- Schedule -':
+            case '- Quality  -':
                 clear();
                 schedule();
-            case '- Grab     -':
+            case '- Range    -':
                 clear();
                 grab();
-            case "6. Update":
+            case "- Special  -":
                 clear();
                 update();
-            case "7. Settings":
+            case "- Exit     -":
                 clear();
-                settings();
-            case "8. Exit":
-                clear();
-                leave();
+                menu();
 
     def update():
         match platform.system:
@@ -117,7 +242,7 @@ def main():
         cursor.hide();
         print("%s%s%s\n%s\n%s\n%s\n%s\n%s" % (bold, cyan, animdl1, animdl2, animdl3, animdl4, animdl5, reset));
         menu = Bullet(
-            prompt = "%s        -Main Menu:- %s \n" % (bold, reset),
+            prompt = "%s                     Main Menu:  %s \n" % (bold, reset),
             choices = [
                 '- Download -',
                 '- Stream   -',
@@ -130,7 +255,7 @@ def main():
             ],
             bullet = "",
             margin = 0,
-            align = 8,
+            align = 20,
             word_on_switch=colors.foreground["white"],
             background_on_switch=colors.background["cyan"]
         )
@@ -152,13 +277,13 @@ def main():
             case '- Grab     -':
                 clear();
                 grab();
-            case "6. Update":
+            case "- Update   -":
                 clear();
                 update();
-            case "7. Settings":
+            case "- Settings -":
                 clear();
                 settings();
-            case "8. Exit":
+            case "- Exit     -":
                 clear();
                 leave();
     menu();
